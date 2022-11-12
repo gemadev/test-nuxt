369bed98f2524e0cd7cc79837a29255652340118
Gonzalo Martinez <gonzalomartinezadalid@gmail.com>
2022-11-12 02:52:52 -0300

    nuxt test

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>About us</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugit
+      temporibus nobis voluptatibus et illum hic architecto officiis totam
+      doloremque laborum, amet provident corrupti facilis fuga? Quasi beatae
+      soluta maxime!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+        title: "Posadas Nuxt || Home",
+        meta: [
+            {
+            hid: "description",
+            name: "description",
+            content: "Pagina web del reserva turistica Posadas",
+            },
+        ],
+        };
+    },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Bienvenido a Posadas</h1>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Posadas Nuxt || Home',
+      meta: [
+        {
+          hid:'description',
+          name:"description",
+          content:"Pagina web del reserva turistica Posadas"
+        }
+      ]
+    }
+  },
+}
+</script>
